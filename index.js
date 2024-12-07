342b862dbb725b580173ef9cc590a4878d87dbaf
AlAminii <alaminbdt50@gmail.com>
2024-12-07 21:14:13 +0600

    all card added

--- index.js
+++ index.js
@@ -13,46 +13,156 @@ function getValueByText (id){
 document.getElementById('first-card-btn').addEventListener('click', function(){
     let firstDonetValue = getInputValueById('first-donet-value')
     let firstDonetMoney = getValueByText('first-donet-money')
-    let total = firstDonetValue + firstDonetMoney;
-let navMoney = getValueByText('nav-money')
-let historySection = document.getElementById('history-section');
-        let historyItem = document.createElement('div');
-        historyItem.classList.add('bg-[#F9F7F3]', 'p-4', 'mb-4', 'rounded', 'shadow-md');
-        historyItem.innerHTML = `
-            <p class="text-xl font-bold">Donation Amount: <span class="text-green-600">${firstDonetValue} BDT</span></p>
-            <p class="text-gray-600">Previous Total: ${firstDonetMoney} BDT</p>
-            <p class="text-gray-600">New Total: ${total} BDT</p>
-        `;
-
-        // Prepend the new history item
-        historySection.prepend(historyItem);
-let nvamoneySet = navMoney - firstDonetValue
-document.getElementById('nav-money').innerText = nvamoneySet
-   document.getElementById('first-donet-money').innerText = total
-   document.getElementById('my_modal_8').classList.add('modal-open');
+    let firstheadlinetext = document.getElementById
+    ('first-card-headline').innerText;
+    let currentDate = new Date();
+let formattedDate = currentDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+    if(firstDonetValue> 0){
+        let total = firstDonetValue + firstDonetMoney;
+        let navMoney = getValueByText('nav-money')
+        let historySection = document.getElementById('history-section');
+                let historyItem = document.createElement('div');
+                historyItem.classList.add('bg-[#F9F7F3]', 'p-4', 'mb-4', 'rounded', 'shadow-md');
+                historyItem.innerHTML = `
+                   <p class="text-xl font-bold">${firstDonetValue} Tk is ${firstheadlinetext} 
+                  
+                    </p>
+                    <p class="text-gray-600"> ${formattedDate}</p>
+                `;
+        
+                // Prepend the new history item
+                historySection.prepend(historyItem);
+        let nvamoneySet = navMoney - firstDonetValue
+        document.getElementById('nav-money').innerText = nvamoneySet
+           document.getElementById('first-donet-money').innerText = total
+           document.getElementById('my_modal_8').classList.add('modal-open');
+    }
+    else{
+        alert('don to value')
+    }
+    
 })
 // ------modal
 document.getElementById('close-modal-btn').addEventListener('click', function() {
     // Hide the modal
     document.getElementById('my_modal_8').classList.remove('modal-open');
   });
-//   modal
 
 
-let historyTab = document.getElementById('history-tab');
-let donetTab = document.getElementById('donation-tab')
-let cardSection = document.getElementById('card-section-from')
-let historySection = document.getElementById('history-section')
-historyTab.addEventListener('click', function(){
-   historyTab.classList.add('bg-[#B4F461]')
-   donetTab.classList.remove('bg-[#B4F461]')
-   cardSection.classList.add('hidden')
-   historySection.classList.remove('hidden')
+document.getElementById('second-card-btn').addEventListener('click', function(){
+    let firstDonetValue = getInputValueById('second-donet-value')
+    let firstDonetMoney = getValueByText('second-donet-money')
+    let secondheadlinetext = document.getElementById
+    ('second-card-headline').innerText;
+    let currentDate = new Date();
+let formattedDate = currentDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+    if(firstDonetValue> 0){
+        let total = firstDonetValue + firstDonetMoney;
+        let navMoney = getValueByText('nav-money')
+        let historySection = document.getElementById('history-section');
+                let historyItem = document.createElement('div');
+                historyItem.classList.add('bg-[#F9F7F3]', 'p-4', 'mb-4', 'rounded', 'shadow-md');
+                historyItem.innerHTML = `
+                     <p class="text-xl font-bold">${firstDonetValue} Tk is ${secondheadlinetext} 
+                  
+                    </p>
+                    <p class="text-gray-600"> ${formattedDate}</p>
+                `;
+        
+                // Prepend the new history item
+                historySection.prepend(historyItem);
+        let nvamoneySet = navMoney - firstDonetValue
+        document.getElementById('nav-money').innerText = nvamoneySet
+           document.getElementById('second-donet-money').innerText = total
+           document.getElementById('my_modal_8').classList.add('modal-open');
+    }
+    else{
+        alert('don to value')
+    }
+    
 })
+// ------modal
+document.getElementById('close-modal-btn').addEventListener('click', function() {
+    // Hide the modal
+    document.getElementById('my_modal_8').classList.remove('modal-open');
+  })
 
-donetTab.addEventListener('click', function(){
-    historyTab.classList.remove('bg-[#B4F461]')
-    donetTab.classList.add('bg-[#B4F461]')
-    cardSection.classList.remove('hidden')
-    historySection.classList.add('hidden')
- })
+//   ---------------3rcard
+
+document.getElementById('third-card-btn').addEventListener('click', function(){
+    let firstDonetValue = getInputValueById('third-donet-value')
+    let firstDonetMoney = getValueByText('third-donet-money')
+    let Thirdheadlinetext = document.getElementById
+    ('third-card-headline').innerText;
+    let currentDate = new Date();
+let formattedDate = currentDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+    if(firstDonetValue> 0){
+        let total = firstDonetValue + firstDonetMoney;
+        let navMoney = getValueByText('nav-money')
+        let historySection = document.getElementById('history-section');
+                let historyItem = document.createElement('div');
+                historyItem.classList.add('bg-[#F9F7F3]', 'p-4', 'mb-4', 'rounded', 'shadow-md');
+                historyItem.innerHTML = `
+                    <p class="text-xl font-bold">${firstDonetValue} Tk is ${Thirdheadlinetext} 
+                  
+                    </p>
+                    <p class="text-gray-600"> ${formattedDate}</p>
+                    
+                `;
+        
+                // Prepend the new history item
+                historySection.prepend(historyItem);
+        let nvamoneySet = navMoney - firstDonetValue
+        document.getElementById('nav-money').innerText = nvamoneySet
+           document.getElementById('third-donet-money').innerText = total
+           document.getElementById('my_modal_8').classList.add('modal-open');
+    }
+    else{
+        alert('positive number input please')
+    }
+    
+})
+// ------modal
+document.getElementById('close-modal-btn').addEventListener('click', function() {
+    // Hide the modal
+    document.getElementById('my_modal_8').classList.remove('modal-open');
+  })
+
+  let historyTab = document.getElementById('history-tab');
+  let donetTab = document.getElementById('donation-tab');
+  let cardSection = document.getElementById('card-section-from');
+  let historySection = document.getElementById('history-section');
+  
+  historyTab.addEventListener('click', function () {
+      // Add background color and text color for the clicked tab
+      historyTab.classList.add('bg-[#B4F461]', 'text-black');
+      donetTab.classList.remove('bg-[#B4F461]', 'text-black');
+      
+      // Toggle visibility of the sections
+      cardSection.classList.add('hidden');
+      historySection.classList.remove('hidden');
+  });
+  
+  donetTab.addEventListener('click', function () {
+      donetTab.classList.add('bg-[#B4F461]', 'text-black');
+      historyTab.classList.remove('bg-[#B4F461]', 'text-black');
+      
+      // Toggle visibility of the sections
+      cardSection.classList.remove('hidden');
+      historySection.classList.add('hidden');
+  });
+  
+
+
